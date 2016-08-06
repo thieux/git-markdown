d95c8b99c9791d95c6a0b7370a1dc832ab536957
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-08-06 15:54:43 +0200

    Print produced markdown to a PDF file using Pandoc.

--- git-markdown.bash
+++ git-markdown.bash
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+function catRevision() {
+  git checkout 2b74b22214bca0264a1f9db9ebea2a3a85efc1c3
+  for f in `find src -iname '*.java'`; do
+    echo $f
+    echo
+    echo '```java'
+    cat "$f" 
+    echo '```'
+    echo
+  done
+}
+
 cd simple-lang
-git checkout 2b74b22214bca0264a1f9db9ebea2a3a85efc1c3
-for f in `find src -iname '*.java'`; do
-  echo $f
-  echo
-  echo '```java'
-  cat "$f" 
-  echo '```'
-  echo
-done
+catRevision > /tmp/article.md
+pandoc /tmp/article.md -s -o /tmp/article.pdf
+evince /tmp/article.pdf&
 
